912571c8e8bbd3a311bb0a46e7dc23ec29c84dc3
dyou <chen.orange@aliyun.com>
2019-08-22 11:35:18 +0800

    change name

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
-import os
+import setuptools
 
-from setuptools import find_packages, setup
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
-setup(
-    name='django-xadmin',
+setuptools.setup(
+    name='django-admin-left-menu',
     version='0.1',
-    packages=find_packages(),
     include_package_data=True,
     license='BSD License',
     description='A simple left menu for DjangoAdmin',
-    url='https://github.com/ChanMo/django-xadmin',
+    long_description=long_description,
+    long_description_context_type='text/markdown',
+    url='https://github.com/ChanMo/django-admin-left-menu',
     author='ChanMo',
     author_email='chan.mo@outlook.com',
+    packages=setuptools.find_packages(),
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
